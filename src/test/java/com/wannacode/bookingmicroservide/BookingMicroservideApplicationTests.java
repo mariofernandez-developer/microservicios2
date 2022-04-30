ccbb22bd03b647698269ed225b37175adff2f681
mariofernandez-developer <facultaddocumentos1@gmail.com>
2022-04-30 16:18:34 -0300

    Microservicios 2

---
+++ src/test/java/com/wannacode/bookingmicroservide/BookingMicroservideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wannacode.bookingmicroservide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMicroservideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
